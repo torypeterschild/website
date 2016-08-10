70dc5d3e25ac7968cc250d7c14e14b84628a1951
torypeterschild <torypeterschild@gmail.com>
2016-08-10 15:46:57 -0400

    more info / less info working, still need to add case for alt collapse link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-$( "a.a-expand" ).click(function() {
-  $( this ).toggleClass( "expanded" );
+
+$( "a.expander" ).click(function() {
+    var domElement = $("#collapseOne")[0] // [0] returns the first DOM element that jQuery found
+    $(domElement.attributes).each(function(index, attribute) {
+        console.log("Attribute:"+attribute.nodeName+" | Value:"+attribute.nodeValue);
+    });
 });
 
-$( "a.a-collapse" ).click(function() {
-  $( "a.a-expand" ).toggleClass( "expanded" );
+$( "#dreamexpand" ).click(function() {
+    $( this ).toggleClass('expander');
+    $( this ).toggleClass('collapsed');
+    if ($( this ).hasClass('expanded')) {
+        $( this ).toggleClass('expanded');
+    } else {
+        $( this ).addClass('expanded');
+    }
+});
+
+$( "#loveexpand" ).click(function() {
+    $( this ).toggleClass('expander');
+    $( this ).toggleClass('collapsed');
+    if ($( this ).hasClass('expanded')) {
+        $( this ).toggleClass('expanded');
+    } else {
+        $( this ).addClass('expanded');
+    }
+});
+
+var domElement = $("#collapseOne")[0] // [0] returns the first DOM element that jQuery found
+$(domElement.attributes).each(function(index, attribute) {
+  console.log("Attribute:"+attribute.nodeName+" | Value:"+attribute.nodeValue);
+});
+
+$( "#dreamexpand" ).click(function() {
+    var domElement = $("#dreamexpand")[0] // [0] returns the first DOM element that jQuery found
+    $(domElement.attributes).each(function(index, attribute) {
+      console.log("Attribute:"+attribute.nodeName+" | Value:"+attribute.nodeValue);
+    });
 });
